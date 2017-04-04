9fccb56cefaa1c1de53d587c5ce23449b4c15908
Roland Rytz <roland.rytz@gmail.com>
2017-04-04 20:12:45 +0200

    Add files I forgot in the last commit

---
+++ redditcrawl.py
@@ -0,0 +1,24 @@
+
+#6y7LtOjoNEfe72g62kZfwtFHMWkQ8XsZvcQ8xZDe
+
+import praw
+
+outfile = open('temp.js', 'w')
+
+credentials = open('credentials', 'r')
+client_id = credentials.readline().strip(' \t\n\r')
+client_secret = credentials.readline().strip(' \t\n\r')
+
+startId = 438
+
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent='atlas_bot')
+
+for submission in reddit.subreddit('placeAtlas').new(limit=1000):
+	if(submission.link_flair_text == "New Entry"):
+		text = submission.selftext
+		text = text.replace("\"id\": 0", "\"id\": "+str(startId))
+		startId = startId + 1
+		outfile.write(text+",")
+		print("written "+submission.title)
+	else:
+		print("skipped "+submission.title)
