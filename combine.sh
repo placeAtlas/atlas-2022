750bee18af1cc1c928f2cf82df7ded256e9b367d
Roland Rytz <roland.rytz@gmail.com>
2017-04-06 12:08:53 +0200

    Reduce server load by combining files, inlining background images and shrinking or indexing images.

---
+++ combine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touch combined.js;
+cat ./web/_js/atlas.js >> combined.js;
+cat ./web/_js/view.js >> combined.js;
+cat ./web/_js/draw.js >> combined.js;
+cat ./web/_js/main.js >> combined.js;
