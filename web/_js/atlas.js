6c69db34b6b3f584347b9f2d9ad3521faabefc0d
Max <m.giller.dev@gmail.com>
2022-04-05 01:46:25 +0200

    Added Hello Internet

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179,"name":"Hello Internet","description":"A podcast by CGP Grey and Dr. Brady Haran.","website":"https://www.hellointernet.fm/","subreddit":"/r/HelloInternet/","center":[69.5,805.5],"path":[[48.5,777.5],[48.5,787.5],[58.5,787.5],[58.5,798.5],[48.5,798.5],[48.5,830.5],[94.5,830.5],[94.5,820.5],[89.5,820.5],[89.5,801.5],[80.5,801.5],[80.5,792.5],[89.5,792.5],[89.5,777.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
