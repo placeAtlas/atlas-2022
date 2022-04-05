4883be20a9fb5e50500c63f76d87a9be9ed897af
Orion <guillaume.turchini@gmail.com>
2022-04-05 02:19:05 +0200

    Adding Shisheyu Mayamoto's community creations

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,8 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179, "name": "Roscoe", "description": "Dog (and emote) of French Twitch streamer Shisheyu_Mayamoto. It's an Eurasier.", "website": "https://www.twitch.tv/shisheyu_mayamoto", "subreddit": "", "center": [ 1928.5, 1677.5 ], "path": [ [ 1907.5, 1659.5 ], [ 1907.5, 1694.5 ], [ 1949.5, 1694.5 ], [ 1949.5, 1659.5 ], [ 1907.5, 1659.5 ] ] },
+{"id":180, "name": "Shisheyu_Mayamoto's Logo", "description": "Shisheyu Mayamoto's Twitch channel's logo", "website": "https://www.twitch.tv/shisheyu_mayamoto", "subreddit": "", "center": [ 1915.5, 1568.5 ], "path": [ [ 1909.5, 1562.5 ], [ 1909.5, 1573.5 ], [ 1921.5, 1573.5 ], [ 1921.5, 1562.5 ], [ 1909.5, 1562.5 ] ] },
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
