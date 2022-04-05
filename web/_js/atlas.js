29ae327202e995b1dbb83325d751ab1ae7956fa2
LizardWizard222 <91688382+LizardWizard222@users.noreply.github.com>
2022-04-05 03:01:37 +0200

    Add 3 Celeste strawberries
    3 of the Strawberries you can find on the game

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 0,"name": "Celeste Strawberries","description": "The 3 different kinds of strawberries in the game Celeste. The first one is a normal winged strawberry, the middle one is the moon berry, available only at the end of Farewell and the third one is the Dashless Golden Strawberry from Chapter 1 A-Side","website": "http://www.celestegame.com","subreddit": "r/celestegame","center": [545.5,23.5],"path": [[508.5,14.5],[570.5,14.5],[584.5,14.5],[562.5,36.5],[542.5,36.5],[535.5,28.5],[514.5,29.5],[508.5,14.5]]}
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
