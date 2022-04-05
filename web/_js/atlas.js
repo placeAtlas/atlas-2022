e0c13bb65a790964966f26cc713fb0ddb5315f6c
Sat42ori <102525012+Sat42ori@users.noreply.github.com>
2022-04-05 03:16:38 +0200

    Update atlas.js
     {
        	"id": 0,
        	"name": "FCK AFD",
        	"description": "Short for \"Fuck AfD\". AfD is a far-right political party in Germany. The 6 letters often find distribution in the form of stickers.",
        	"website": "https://www.parteibedarf.de/50-FCK-AFD-Aufkleber/A-11",
        	"subreddit": "",
        	"center": [
        		335.5,
        		836.5
        	],
        	"path": [
        		[
        			296.5,
        			830.5
        		],
        		[
        			372.5,
        			830.5
        		],
        		[
        			372.5,
        			842.5
        		],
        		[
        			297.5,
        			841.5
        		]
        	]
        }

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,35 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+ {
+    	"id": 0,
+    	"name": "FCK AFD",
+    	"description": "Short for \"Fuck AfD\". AfD is a far-right political party in Germany. The 6 letters often find distribution in the form of stickers.",
+    	"website": "https://www.parteibedarf.de/50-FCK-AFD-Aufkleber/A-11",
+    	"subreddit": "",
+    	"center": [
+    		335.5,
+    		836.5
+    	],
+    	"path": [
+    		[
+    			296.5,
+    			830.5
+    		],
+    		[
+    			372.5,
+    			830.5
+    		],
+    		[
+    			372.5,
+    			842.5
+    		],
+    		[
+    			297.5,
+    			841.5
+    		]
+    	]
+    }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
