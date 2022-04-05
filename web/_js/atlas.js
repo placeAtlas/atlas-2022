d636c6f6b93f23d3d2a8e991fb18819709b74687
EpicGamer2469 <62869918+Epicgamer2469@users.noreply.github.com>
2022-04-04 19:02:36 -0500

    Add WYS Shelly (Small)

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,6 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
+{"id":179, "name": "Shelly (Small)", "description": "Shelly is the player character in the indie platformer Will You Snail?", "website": "https://store.steampowered.com/app/1115050/Will_You_Snail/", "subreddit": "/r/WillYouSnail", "center": [ 853.5, 973.5 ], "path": [ [ 849.5, 971.5 ], [ 849.5, 971.5 ], [ 850.5, 971.5 ], [ 851.5, 971.5 ], [ 851.5, 970.5 ], [ 857.5, 970.5 ], [ 857.5, 976.5 ], [ 849.5, 976.5 ], [ 849.5, 973.5 ] ] },
 
 ];
 
