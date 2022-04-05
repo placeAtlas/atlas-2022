c38108ab6b1c32328785c1287751196b36a01bae
Emma C. Pointer-Null <emmaffle@modrinth.com>
2022-04-04 20:30:00 -0400

    Add Modrinth

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,6 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
+{"id":179,"name":"Modrinth","description":"The logo for Modrinth, the open source modding platform.","website":"https://modrinth.com","subreddit":"","center":[730.5,642.5],"path":[[727.5,646.5],[727.5,645.5],[726.5,645.5],[726.5,644.5],[725.5,644.5],[725.5,639.5],[726.5,639.5],[726.5,638.5],[727.5,638.5],[727.5,637.5],[732.5,637.5],[732.5,638.5],[733.5,638.5],[733.5,639.5],[734.5,639.5],[734.5,644.5],[733.5,644.5],[733.5,645.5],[732.5,645.5],[732.5,646.5],[727.5,646.5]]},
 
 ];
 
