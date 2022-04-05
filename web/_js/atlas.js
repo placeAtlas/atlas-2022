d76b5eb34ed508d2b2b8eebba041ee18b8fc73e2
Alexa <asl70@pitt.edu>
2022-04-04 21:07:24 -0400

    University of Pittsburgh

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 0, "name": "Pitt Logo and the Cathedral of Learning", "description": "The University of Pittsburgh script emblem to the left of the main university building, the Cathedral of Learning. It's the largest educational building in the western hemisphere.", "website": "https://pitt.edu", "subreddit": "/r/pitt", "center": [1764.5, 622.5], "path": [[1744.5, 607.5], [1784.5, 607.5], [1784.5, 636.5], [1744.5, 636.5], [1744.5, 607.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
