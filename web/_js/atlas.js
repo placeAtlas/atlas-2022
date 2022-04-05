64f7c3a10de0fd4af4c57802ca765bef9f05aec7
Aeonimous <103009836+Aeonimous@users.noreply.github.com>
2022-04-04 21:46:37 -0400

    Add WYSI
    See: https://www.reddit.com/r/placeAtlas2/comments/twh4st/wysi_new_entry/

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 0, "name": "WYSI", "description": "The emote WYSI is based off a popular clip of the osu! player Aireu shouting \"WHEN YOU SEE IT\" and is often used whenever the number \"727\" is spotted.", "website": "https://www.youtube.com/watch?v=AaAF51Gwbxo", "subreddit": "/r/osugame", "center": [ 626.5, 1489.5 ], "path": [ [ 660.5, 1453.5 ], [ 660.5, 1522.5 ], [ 592.5, 1522.5 ], [ 592.5, 1457.5 ], [ 640.5, 1457.5 ], [ 640.5, 1456.5 ], [ 642.5, 1456.5 ], [ 642.5, 1453.5 ] ] }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
