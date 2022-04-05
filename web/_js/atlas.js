4e462f9f4bba5f7862381b9152f85f545c4c9ca3
ynot01 <ynot000001@gmail.com>
2022-04-04 20:52:22 -0400

    Update atlas.js

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{ "id": 0, "name": "Zero (Katana ZERO)", "description": "Zero, from the video game Katana ZERO", "website": "https://katana-zero.fandom.com/wiki/Zero", "subreddit": "/r/KatanaZero", "center": [ 1150.5, 1875.5 ], "path": [ [ 1137.5, 1860.5 ], [ 1137.5, 1893.5 ], [ 1158.5, 1893.5 ], [ 1158.5, 1889.5 ], [ 1164.5, 1889.5 ], [ 1164.5, 1858.5 ], [ 1137.5, 1858.5 ] ] },
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
