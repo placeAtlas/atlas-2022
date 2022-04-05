fae9d8089877dcbc500ac171dfa1f261f695b4c1
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2022-04-05 09:12:07 +0700

    Added both Flag of Thailand
    Yes, there are 2 Flags of Thailand with different stuff in it.

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,9 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":0,"name":"Flag of Thailand","description":"This is the first flag of Thailand that was made during the first expansion and has since been invaded a few times but never been overtaken completely. It featured a silhouette of the country and an elephant, Thailand's national animal.","website":"","subreddit":"r/thaithai, r/thailand","center":[1485.5,752.5],"path":[[1461.5,740.5],[1461.5,764.5],[1507.5,764.5],[1507.5,746.5],[1515.5,746.5],[1518.5,743.5],[1518.5,742.5],[1516.5,740.5],[1515.5,741.5],[1514.5,741.5],[1514.5,740.5]]},
+{"id":0,"name":"Flag of Thailand","description":"This is the second (albeit smaller) flag of Thailand that was made after the second expansion. It featured a glass of Thai Tea and the Democracy Monument.","website":"","subreddit":"r/thaithai, r/thailand","center":[503.5,1807.5],"path":[[491.5,1801.5],[491.5,1813.5],[515.5,1813.5],[515.5,1801.5]]}
+	
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
