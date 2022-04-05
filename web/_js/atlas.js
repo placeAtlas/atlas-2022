56b7c4b8197e4bc5237873feb04450ea3d582f9f
Nate Sternberg <69009690+theothertallguy@users.noreply.github.com>
2022-04-04 18:54:17 -0700

    Add UBC Crest

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,8 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
-];
+{"id":179,"name":"UBC Crest","description":"The crest of the University of British Columbia, a Canadian post-secondary institution located in Vancouver, BC and Kelowna, BC.","website":"","subreddit":"r/UBC","center":[184.5,540.5],"path":[[175.5,528.5],[193.5,528.5],[193.5,552.5],[175.5,552.5]]}];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
 
