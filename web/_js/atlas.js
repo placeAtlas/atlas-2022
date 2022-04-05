338f66cd233db93d78bb9d856a9d04cf59c2b887
Isse <41273856+IsseW@users.noreply.github.com>
2022-04-05 02:21:51 +0200

    Add the game veloren

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,6 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
+{"id":179, "name": "Veloren", "description": "Veloren is an open source multiplayer voxel RPG written in Rust.", "website": "https://veloren.net/", "subreddit": "/r/veloren", "center": [ 741.5, 642.5 ], "path": [ [ 736.5, 637.5 ], [ 745.5, 637.5 ], [ 745.5, 646.5 ], [ 736.5, 646.5 ], [ 736.5, 646.5 ] ] }
 
 ];
 
