2154456fceb59420c30995674eafb3d9f5bb557e
ninjuh1124 <32426314+ninjuh1124@users.noreply.github.com>
2022-04-04 23:17:28 -0400

    Add Jelledot

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 0,"name": "jelLove","description": "Emote for Twitch stream Jelledot","website": "https://www.twitch.tv/jelledot","subreddit": "","center": [1958.5,748.5],"path": [[1953.5,744.5],[1953.5,752.5],[1962.5,752.5],[1962.5,744.5],[1953.5,744.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
