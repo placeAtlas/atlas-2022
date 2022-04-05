e6271313a20085b52626e94aeaff67f446c68e66
Peter Benko <PeterBenko@users.noreply.github.com>
2022-04-05 03:15:29 +0200

    Added entry for the hayrack

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{ "id": 0, "name": "Hayrack", "description": "A hayrack (Slovene: kozolec) is a freestanding vertical drying rack found chiefly in Slovenia.", "website": "https://en.wikipedia.org/wiki/Hayrack", "subreddit": "/r/slovenia", "center": [ 227.5, 881.5 ], "path": [ [ 218.5, 874.5 ], [ 218.5, 887.5 ], [ 235.5, 887.5 ], [ 235.5, 874.5 ] ] }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
