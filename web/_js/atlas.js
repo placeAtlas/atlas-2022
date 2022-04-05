d10f8d9d64bcb62d0cc67056f9ac0b63191c509a
FermiD <55369111+FermiD@users.noreply.github.com>
2022-04-05 12:56:06 +0700

    Update atlas.js

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-{"id": 0, "name": "Little Princess", "description": "The Little Princess from Guardian Tales, she gives you gifts daily in Heavenhold.", "website": "", "subreddit": "r/guardiantales", "center": [ 702.5, 225.5 ], "path": [ [701.5, 213.5], [694.5, 220.5], [694.5, 234.5], [709.5, 234.5], [709.5, 220.5], [702.5, 213.5]]},
+{"id":179, "name": "Little Princess", "description": "The Little Princess from Guardian Tales, she gives you gifts once every day in Heavenhold.", "website": "", "subreddit": "r/guardiantales", "center": [ 702.5, 225.5 ], "path": [ [701.5, 213.5], [694.5, 220.5], [694.5, 234.5], [709.5, 234.5], [709.5, 220.5], [702.5, 213.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
