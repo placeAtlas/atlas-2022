59f61b623fd96a90090a7a0f4a06448faf3764d9
Ty <42102494+Endatronix@users.noreply.github.com>
2022-04-04 20:51:31 -0600

    Add CU Boulder

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,8 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
-];
+{"id": 0,"name": "University of Colorado Boulder","description": "Logo of the best college in Colorado. Sko buffs!","website": "https://colorado.edu","subreddit": "r/cuboulder","center": [16.5,727.5],"path": [[12.5,723.5],[19.5,723.5],[19.5,730.5],[12.5,730.5],[12.5,730.5]]}];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
 
