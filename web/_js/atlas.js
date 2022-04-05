22bcc65bfb8551e3dbf53ee965a4a4db2240db75
PhlegethonAcheron <53194290+PhlegethonAcheron@users.noreply.github.com>
2022-04-04 22:51:26 -0400

    Added Syracuse University

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,8 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
-];
+{"id":0, "name": "Syracuse University", "description": "Syracuse University, a Private university in Syracuse, NY. The pixel art includes the \"S\" and the Carrier Dome", "website": "https://www.syracuse.edu/", "subreddit": "/r/SyracuseU", "center": [ 1769.5, 560.5 ], "path": [ [ 1765.5, 551.5 ], [ 1774.5, 551.5 ], [ 1774.5, 568.5 ], [ 1766.5, 568.5 ], [ 1762.5, 562.5 ], [ 1765.5, 562.5 ] ] }
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
 
