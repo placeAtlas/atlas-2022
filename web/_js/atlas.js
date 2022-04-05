c3ba2b779e4eb078b9b32fa12f82efb08d095172
Marcelorar <marceloecheverria23@gmail.com>
2022-04-04 20:21:13 -0500

    Adding Ecuador Entry
    + Ecuador Location in r/place.

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,8 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
-];
+{"id": 179,"name": "Ecuador", "description": "South America Country.", "website": "https://es.wikipedia.org/wiki/Ecuador","subreddit": "/r/ecuador","center": [1607.5,587.5],"path": [[1549.5,568.5],[1574.5,569.5],[1577.5,569.5],[1577.5,578.5],[1577.5,579.5],[1678.5,580.5],[1678.5,598.5],[1549.5,598.5]]}];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
 
