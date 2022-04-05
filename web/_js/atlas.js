ff72293b5438e36bc81e85ab06d7b961d40ddd6f
DerBerliner <34981166+DerBerlinr@users.noreply.github.com>
2022-04-05 02:47:29 +0200

    Added TU Berlin logo

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":180, "name": "TU Berlin", "description": "The logo of the Technical University Berlin with a bad connection sign, because they had recent problems with cyber attacks. ", "website": "https://www.tu.berlin/", "subreddit": "/r/tuberlin", "center": [1400.5, 820.5], "path": [[1392.5, 806.5], [1392.5, 830.5], [1411.5, 830.5], [1411.5, 817.5], [1401.5, 817.5], [1401.5, 806.5], [1392.5, 806.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
