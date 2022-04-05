80cd9a79068989cde1c1d2bea3bf149d1a8d3f9e
Alex Bronner <acbronn@gmail.com>
2022-04-04 20:29:15 -0400

    Updated atlas.js - "MechWarrior Timber Wolf"
    (Why do these updates go through git? Surely you can create a more user & developer friendly process.)

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179,"name": "MechWarrior Timber Wolf (Mad Cat)", "description": "A depiction of a Timber Wolf (Mad Cat) from MechWarrior and BattleTech.\n\nBased on the .exe icon for MechWarrior 3, and the explosion background on the MechWarrior 2 box art.", "website": "", "subreddit": "/r/MechWarrior", "center": [1348.5, 533.5], "path": [[1339.5, 522.5], [1339.5, 543.5], [1357.5, 543.5], [1357.5, 522.5]] }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
