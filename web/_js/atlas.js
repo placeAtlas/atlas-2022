06fb2f7d83b277d245072ef3c22cf69227ac0ea1
ryanzzz1243 <ryry2019@gmail.com>
2022-04-04 20:22:56 -0500

    Add starcraft 2

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179, "name": "Starcraft 2", "description": "Starcraft 2 is an RTS game by Blizzard", "website": "", "subreddit": "r/starcraft", "center": [656.5, 1832.5], "path": [[642.5, 1818.5], [642.5, 1846.5], [670.5, 1846.5], [670.5, 1818.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
