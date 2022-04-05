3ed6b277239ea6b9f1a86813c1982687eb3933a3
ninjuh1124 <32426314+ninjuh1124@users.noreply.github.com>
2022-04-04 23:06:28 -0400

    Add Kyoto Animation

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 0,"name": "Kyoto Animation Logo and Characters","description": "Haruhi Suzumiya, Eru Chitanda, Tohru (later replaced with Konata Izumi), Yui Hirasawa","website": "","subreddit": "/r/kyoani","center": [1925.5,759.5],"path": [[1904.5,744.5],[1919.5,744.5],[1919.5,760.5],[1953.5,760.5],[1953.5,769.5],[1920.5,769.5],[1920.5,760.5],[1904.5,760.5],[1904.5,744.5]]}
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
