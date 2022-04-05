522bc41635af73b71ab72ba0d4c7fa1a18032761
Torktumlaren <tisser44@gmail.com>
2022-04-05 02:04:39 +0200

    Add TeosGame

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,8 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
-];
+{"id":179, "name": "TeosGame", "description": "Mural made by the TeosGame community.", "website": "https://www.youtube.com/teosgame", "subreddit": "/r/TeosGame", "center": [1122.5,1000.5], "path": [[1083.5,962.5], [1083.5,1038.5], [1160.5,1038.5], [1160.5,962.5]]}];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
 
