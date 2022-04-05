71f42e45b825d028d930cf313146e401627ed72a
AlephCubed <76791009+AlephCubed@users.noreply.github.com>
2022-04-04 18:54:33 -0700

    Added Hypixel Logo

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,87 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+    {
+    	"id": 179,
+    	"name": "Hypixel Logo",
+    	"description": "Hypixel is one of the largest Minecraft servers focused on minigames. It is also connected to Hypixel Studios the developer of the sandbox game Hytale. ",
+    	"website": "https://hypixel.net",
+    	"subreddit": "/r/Hypixel",
+    	"center": [
+    		1380.5,
+    		1851.5
+    	],
+    	"path": [
+    		[
+    			1399.5,
+    			1819.5
+    		],
+    		[
+    			1399.5,
+    			1821.5
+    		],
+    		[
+    			1392.5,
+    			1835.5
+    		],
+    		[
+    			1392.5,
+    			1864.5
+    		],
+    		[
+    			1394.5,
+    			1865.5
+    		],
+    		[
+    			1394.5,
+    			1869.5
+    		],
+    		[
+    			1382.5,
+    			1881.5
+    		],
+    		[
+    			1379.5,
+    			1889.5
+    		],
+    		[
+    			1376.5,
+    			1889.5
+    		],
+    		[
+    			1366.5,
+    			1873.5
+    		],
+    		[
+    			1366.5,
+    			1870.5
+    		],
+    		[
+    			1368.5,
+    			1868.5
+    		],
+    		[
+    			1368.5,
+    			1839.5
+    		],
+    		[
+    			1362.5,
+    			1832.5
+    		],
+    		[
+    			1376.5,
+    			1823.5
+    		],
+    		[
+    			1379.5,
+    			1823.5
+    		],
+    		[
+    			1388.5,
+    			1819.5
+    		]
+    	]
+    }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
