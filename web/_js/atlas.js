074848d24d53961138a4391bd1902ae0a02e5c60
Ahmet <35678867+ahmetmutlugun@users.noreply.github.com>
2022-04-04 18:24:01 -0700

    Added Stardew Valley Duck next to ratge

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,6 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
+{"id":179,"name":"Stardew Valley Duck","description":"Stardew Valley duck sitting on the shoulders of ratge. ","website":"https://www.stardewvalley.net","subreddit":"r/stardewvalley","center":[1700.5,993.5],"path":[[1699.5,984.5],[1705.5,984.5],[1705.5,985.5],[1706.5,985.5],[1706.5,987.5],[1707.5,987.5],[1707.5,998.5],[1706.5,998.5],[1706.5,1000.5],[1696.5,1000.5],[1696.5,999.5],[1695.5,999.5],[1695.5,998.5],[1694.5,998.5],[1693.5,997.5],[1693.5,996.5],[1692.5,996.5],[1691.5,995.5],[1691.5,992.5],[1690.5,992.5],[1690.5,991.5],[1692.5,991.5],[1693.5,990.5],[1694.5,990.5],[1695.5,989.5],[1696.5,988.5],[1697.5,987.5],[1698.5,986.5],[1699.5,985.5],[1699.5,984.5]]},
 
 ];
 
