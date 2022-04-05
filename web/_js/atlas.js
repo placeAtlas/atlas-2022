65bcdfba9cfcd7193eee292776792b3077f854be
Shaemus Melvin <46724899+smelv384@users.noreply.github.com>
2022-04-04 18:22:49 -0600

    Add Esperanto
    I apologize in advance for the ID conflicts you'll have to deal with.  Thanks for making the atlas!

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179,"name": "Esperanto","description": "A proposed international language designed to be as simple as possible to understand","website": "https://esperanto.net/en/","subreddit": "/r/esperanto","center": [1466.5,494.5],"path": [[1433.5,487.5],[1499.5,487.5],[1499.5,501.5],[1433.5,501.5]]}
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
