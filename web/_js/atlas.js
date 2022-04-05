945d495146b1bfae62f3a16c5c6200172352a5af
ningmenhao <purelymadgenius@gmail.com>
2022-04-05 00:27:26 +0000

    Add xPowers to the atlas

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id": 179, "name": "r/xPowers", "description": "A geopolitical subreddit", "website": "", "subreddit": "/r/xPowers", "center": [884.5, 1484.5], "path": [[882.5, 1460.5], [884.5, 1460.5], [881.5, 1460.5], [887.5, 1460.5], [880.5, 1459.5], [888.5, 1460.5], [887.5, 1511.5], [881.5, 1511.5], [881.5, 1511.5], [880.5, 1459.5]]},
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
