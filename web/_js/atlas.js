8032e89fce9c333b9f1c66d0af8f43f381b981d7
Mulverine <mulverin3@gmail.com>
2022-04-04 18:49:20 -0600

    Fix spelling

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-{"id":179,"name": "Minecraft Commands","description": "Welcome to Minecraft Commands! We're a community focussed around vanilla Minecraft commands and mapmaking","website": "https://discord.gg/QAFXFtZ","subreddit": "/r/MinecraftCommands","center": [ 330.5, 1592.5 ], "path": [ [ 323.5, 1585.5 ], [ 337.5, 1585.5 ], [ 337.5, 1599.5 ], [ 323.5, 1599.5 ], [ 323.5, 1585.5 ] ] },
+{"id":179,"name": "Minecraft Commands","description": "Welcome to Minecraft Commands! We're a community focused around vanilla Minecraft commands and mapmaking.","website": "https://discord.gg/QAFXFtZ","subreddit": "/r/MinecraftCommands","center": [ 330.5, 1592.5 ], "path": [ [ 323.5, 1585.5 ], [ 337.5, 1585.5 ], [ 337.5, 1599.5 ], [ 323.5, 1599.5 ], [ 323.5, 1585.5 ] ] },
 
 ];
 
