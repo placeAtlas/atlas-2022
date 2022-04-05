fc202fc38fb1fe40fd060cf31a6e273aee6122c5
AlephCubed <76791009+AlephCubed@users.noreply.github.com>
2022-04-04 20:22:34 -0700

    Added Deltarune Plushies

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,55 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+    {
+    	"id": 179,
+    	"name": "Deltarune Plushies",
+    	"description": "Plushies from the role playing game Deltarune. On the top row is Susie and Spamton, and on the bottom is Berdly, Ralsei, Noelle, and Kris.",
+    	"website": "https://deltarune.com",
+    	"subreddit": "/r/deltarune",
+    	"center": [
+    		1774.5,
+    		186.5
+    	],
+    	"path": [
+    		[
+    			1745.5,
+    			161.5
+    		],
+    		[
+    			1776.5,
+    			161.5
+    		],
+    		[
+    			1776.5,
+    			172.5
+    		],
+    		[
+    			1803.5,
+    			172.5
+    		],
+    		[
+    			1805.5,
+    			174.5
+    		],
+    		[
+    			1805.5,
+    			206.5
+    		],
+    		[
+    			1748.5,
+    			206.5
+    		],
+    		[
+    			1748.5,
+    			174.5
+    		],
+    		[
+    			1745.5,
+    			174.5
+    		]
+    	]
+    }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
