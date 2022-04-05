f611729cb8e2e7e30e3476a0967022abc19c931d
nekiwo <58634064+nekiwo@users.noreply.github.com>
2022-04-04 22:24:12 -0500

    Added Marisad

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-
+{"id":179,"name":"Marisad","description":"Marisad is popular GIF featuring a Touhou character named Marisa. It has grown in popularity through a Discord server dedicated to this image.","website":"https://discord.gg/99Q8FsP","subreddit":"/r/marisad","center":[1799.5,882.5],"path":[[1786.5,870.5],[1786.5,889.5],[1789.5,891.5],[1790.5,895.5],[1807.5,895.5],[1811.5,895.5],[1811.5,870.5],[1786.5,870.5]]}
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
