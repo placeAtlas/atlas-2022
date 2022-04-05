11545d3b8d77456161c93205e46f917bf24352c4
xMatador <57844715+xMatador-Git@users.noreply.github.com>
2022-04-05 04:02:47 +0200

    Adding Moondye7,NNO,Noway4u_sir
    Adding Moondye7 logo,NNO logo ,Noway4u_sir twitch emote

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,6 +226,9 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
+{"id":179,"name":"Moondye7 logo","description":"Logo from a german streamer called Moondye7","website":"https://www.twitch.tv/moondye7","subreddit":"","center":[848.5,995.5],"path":[[843.5,992.5],[843.5,998.5],[853.5,998.5],[853.5,992.5],[843.5,992.5]]}
+{"id":180,"name":"NNO logo","description":"The logo of the german League of Legends E-Sport team \"NoNeedOrga\"","website":"https://twitter.com/NNO_V10","subreddit":"","center":[651.5,1986.5],"path":[[624.5,1976.5],[677.5,1976.5],[677.5,1995.5],[624.5,1995.5],[624.5,1976.5]]}
+{"id":181,"name":"Noway4u_sir's twitch emote","description":"Noway4u_sir's twitch emote placed on r/place by his community\n","website":"https://www.twitch.tv/noway4u_sir","subreddit":"","center":[608.5,1985.5],"path":[[597.5,1975.5],[597.5,1995.5],[619.5,1995.5],[619.5,1974.5]]}
 
 ];
 
