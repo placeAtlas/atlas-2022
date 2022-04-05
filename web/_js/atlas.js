78b33757b30c5dd34f82fad4f358770de6474fb2
Blokyk <32983140+Blokyk@users.noreply.github.com>
2022-04-05 02:17:58 +0200

    Edited description

--- web/_js/atlas.js
+++ web/_js/atlas.js
@@ -226,7 +226,7 @@ var atlas = [
 {"id":176,"name": "Interlingue", "description": "Flag of Interlingue, an IAL to communicate between Western European languages.", "website": "https://occidental-lang.com", "subreddit": "/r/interlingue", "center": [ 768.5, 328.5 ], "path": [ [ 765.5, 326.5 ], [ 770.5, 326.5 ], [ 770.5, 329.5 ], [ 765.5, 329.5 ] ] },
 {"id":177, "name": "Viossa", "description": "Flag of the conpidgin Viossa.", "website": "", "subreddit": "/r/viossa", "center": [ 775.5, 296.5 ], "path": [ [ 769.5, 290.5 ], [ 780.5, 290.5 ], [ 780.5, 301.5 ], [ 769.5, 301.5 ] ] },
 {"id":178, "name": "Lojban", "description": "Flag of Lojban", "website": "https://lojban.org", "subreddit": "/r/lojban", "center": [ 760.5, 328.5 ], "path": [ [ 763.5, 326.5 ], [ 757.5, 326.5 ], [ 757.5, 330.5 ], [ 763.5, 330.5 ] ] },
-{"id":179, "name": "Rhythm Doctor - Otto", "description": "A character from the incredible game \"Rhythm Doctor.\" It's especially linked with the discord/lounge, known to the community under the RDL acronym", "website": "https://rhythmdr.com/", "subreddit": "", "center": [ 77.5, 710.5 ], "path": [ [ 72.5, 704.5 ], [ 82.5, 704.5 ], [ 82.5, 715.5 ], [ 72.5, 715.5 ] ] }
+{"id":179, "name": "Rhythm Doctor - Otto", "description": "The auto-play robot Otto, from the rhythm game Rhythm Doctor. RDL is the acronym for the community discord, Rhythm Doctor Lounge", "website": "https://rhythmdr.com/", "subreddit": "", "center": [ 77.5, 710.5 ], "path": [ [ 72.5, 704.5 ], [ 82.5, 704.5 ], [ 82.5, 715.5 ], [ 72.5, 715.5 ] ] }
 ];
 
 //console.log("There are "+atlas.length+" entries in the Atlas.");
