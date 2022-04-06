12610b7a7478da4596f5f23b8208b0397af61c2f
Hans5958 <hans5958@outlook.com>
2022-04-06 08:20:24 +0700

    Improve reduce markdown links script

--- tools/less-md-links.py
+++ tools/less-md-links.py
@@ -5,19 +5,25 @@ pattern = re.compile(r'\[(.+?)\]\((.+?)\)')
 
 def go(path):
 
+  print(f"Fixing {path}...")
+
   with open(path, "r+", encoding='UTF-8') as f1:
     contents = f1.read()
 
+  print("Stage 1 completed.")
   for match in pattern.finditer(contents):
     if match.group(1) == match.group(2):
       contents = contents.replace(match.group(0), match.group(2), 1)
 
+  print("Stage 2 completed.")
   for match in pattern.finditer(contents):
     if match.group(1) == match.group(2):
       contents = contents.replace(match.group(0), match.group(2), 1)
 
   with open(path, "w", encoding='UTF-8') as f2:
     f2.write(contents)
+  print("Writing completed. All done.")
+
 
 go("../web/atlas.json")
 go("../web/atlas-before-ids-migration.json")
