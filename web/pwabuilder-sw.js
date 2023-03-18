321731f6b4b7d037c14ac120949e47a2969b9413
Hans5958 <hans5958@outlook.com>
2023-03-18 08:58:34 +0700

    Update PWA scripts

--- web/pwabuilder-sw.js
+++ web/pwabuilder-sw.js
@@ -3,7 +3,7 @@
 const CACHE = "pwabuilder-offline";
 const QUEUE_NAME = "bgSyncQueue";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://cdn.jsdelivr.net/npm/workbox-sw@6.5.4/build/workbox-sw.min.js');
 
 self.addEventListener("message", (event) => {
     if (event.data && event.data.type === "SKIP_WAITING") {
