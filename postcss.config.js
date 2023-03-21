480918705400a05f8edad6d8248d8981ba6f0ab4
Hans5958 <hans5958@outlook.com>
2023-03-21 19:32:49 +0700

    Simplify postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,14 @@
 const plugins = [];
 
 if (process.env.NODE_ENV === "production") {
-  plugins.push(
-    require("@fullhuman/postcss-purgecss")({
+  plugins.push({
+    "@fullhuman/postcss-purgecss": {
       content: [
-        './dist-temp/*.html', 
-        './dist-temp/**/*.html', 
-        './dist-temp/**/*.js',
-        './dist-temp/*.svg', 
-        './dist-temp/**/*.svg'
+        './dist-temp/*.{html,js,svg}', 
+        './dist-temp/**/*.{html,js,svg}' 
       ]
-    })
-  );
+    }
+  })
 }
 
 module.exports = {
