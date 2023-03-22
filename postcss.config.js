c0ad17651944729589d4581fbc3f56d1f25acec4
Hans5958 <hans5958@outlook.com>
2023-03-22 21:42:43 +0700

    Fix PostCSS config, convert files to tab

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,14 @@
 const plugins = [];
 
 if (process.env.NODE_ENV === "production") {
-  plugins.push({
-    "@fullhuman/postcss-purgecss": {
-      content: [
-        './dist-temp/*.{html,js,svg}', 
-        './dist-temp/**/*.{html,js,svg}' 
-      ]
-    }
-  })
-}
+	plugins["@fullhuman/postcss-purgecss"] = {
+			content: [
+				'./dist-temp/*.{html,js,svg}', 
+				'./dist-temp/**/*.{html,js,svg}' 
+			]
+		}
+	}
 
 module.exports = {
-  plugins
+	plugins
 };
