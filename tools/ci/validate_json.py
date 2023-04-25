bfb99e155da9a5b81a8c84ac72d59a8790ec69c8
Hans5958 <hans5958@outlook.com>
2023-04-25 13:19:52 +0700

    Add schema-based validation

--- tools/ci/validate_json.py
+++ tools/ci/validate_json.py
@@ -2,13 +2,23 @@
 
 import sys
 import json 
+from jsonschema import validate
 
-path = "./../../web/atlas.json"
+atlasPath = "./../../web/atlas.json"
 
 # path override as 1st param: validate_json.py path_to_file.json
 if (len(sys.argv) > 1):
-    path = sys.argv[1]
+    atlasPath = sys.argv[1]
 
-json.load(open(path, "r", encoding='utf-8'))
+schemaPath = "./../atlas.schema.json"
+
+# schema override as 2nd param: validate_json.py [...] path_to_schema.json
+if (len(sys.argv) > 2):
+    schemaPath = sys.argv[2]
+
+atlas = json.load(open(atlasPath, "r", encoding='utf-8'))
+schema = json.load(open(schemaPath, "r", encoding='utf-8'))
+
+validate(atlas, schema)
 
 print("JSON is valid")
